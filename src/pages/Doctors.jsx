38b46cc14b3ddc1efcc0fe5893aed2972a575dbe
MoKhaledAmin <mokhaledamin123@gmail.com>
2024-10-24 14:18:41 +0300

    Handle Filter

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -10,6 +10,7 @@ const Doctors = () => {
     const Navigate = useNavigate();
     const { speciality } = useParams();
     const { doctors } = useContext(AppContext);
+    const [showFilter, setShowFilter] = useState(false);
     const [filterDoctor, setFilterDoctor] = useState([]);
 
     // Filter Doctors
@@ -32,7 +33,8 @@ const Doctors = () => {
             <div>
                 <p className='text-gray-600'>Browse Through The Doctors Specialist.</p>
                 <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-                    <div className='flex flex-col gap-4 text-sm text-gray-600'>
+                    <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`} onClick={() => setShowFilter(prev => !prev)}>Filters</button>
+                    <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
                         <p onClick={() => speciality === 'General physician' ? Navigate('/doctors') : Navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'General physician' ? 'bg-indigo-100 text-black' : ''}`}>General Physician</p>
                         <p onClick={() => speciality === 'Gynecologist' ? Navigate('/doctors') : Navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gynecologist' ? 'bg-indigo-100 text-black' : ''}`}>Gynecologist</p>
                         <p onClick={() => speciality === 'Dermatologist' ? Navigate('/doctors') : Navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Dermatologist' ? 'bg-indigo-100 text-black' : ''}`}>Dermatologist</p>
